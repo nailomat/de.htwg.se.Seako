e54fa86482774b0e1953f05a9122663a299768fe
Andreas Ly <andreas.ly96@gmail.com>
2020-05-22 08:31:39 +0200

    SK007 Write Grid and first test implementation for Grid

---
+++ src/test/scala/de/htwg/se/seako/model/GridSpec.scala
@@ -0,0 +1,14 @@
+package de.htwg.se.seako.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class GridSpec extends WordSpec with Matchers {
+  val cell = Cell(1)
+  "A Grid" when {
+
+    "has not any Cells" should {
+      //val NoGrid = Grid(Vector[Vector[Cell]])
+    }
+  }
+
+}
