0e9233965093699d5881813ea213a25c5c3835a5
Andreas Ly <andreas.ly96@gmail.com>
2020-05-29 08:44:08 +0200

    Create tui in main

--- src/main/scala/de/htwg/se/seako/Seako.scala
+++ src/main/scala/de/htwg/se/seako/Seako.scala
@@ -1,16 +1,21 @@
 package de.htwg.se.seako
 
+import de.htwg.se.seako.aview.Tui
 import de.htwg.se.seako.model.{Cell, Grid, Player}
 
 object Seako {
   def main(args: Array[String]): Unit = {
-    val student = Player("Your Name")
-    println("Hello, " + student.name)
-    println("Seako")
-    val playingField = new Grid[Cell](2,Cell(1))
-    println(playingField.cell(0,1))
-    val newPlayingField = playingField.replaceCell(0,1,Cell(2))
-    println(newPlayingField.cell(0,1))
-
+    var tui = new Tui()
+//    val playingField = new Grid[Cell](2,Cell(1))
+//    println(playingField.cell(0,1))
+//    val newPlayingField = playingField.replaceCell(0,1,Cell(2))
+//    println(newPlayingField.cell(0,1))
+    var input: String = ""
+    if (args.length > 0) input = args(0)
+    if (!input.isEmpty) tui.processInputLine(input)
+    else do {
+      input = readLine()
+      tui.processInputLine(input)
+    } while (input != "q")
   }
 }
