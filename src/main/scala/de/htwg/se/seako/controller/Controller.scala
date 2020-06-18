9e4f27d1630612f981ff455c52636abeb217e0f5
Andreas Ly <andreas.ly96@gmail.com>
2020-06-18 15:35:32 +0200

    SK023 - Add PlayerList

--- src/main/scala/de/htwg/se/seako/controller/Controller.scala
+++ src/main/scala/de/htwg/se/seako/controller/Controller.scala
@@ -1,23 +1,17 @@
 package de.htwg.se.seako.controller
 
+import de.htwg.se.seako.controller.GameStatus._
 import de.htwg.se.seako.model._
 import de.htwg.se.seako.util.Observable
 
 class Controller(var grid: Grid[Cell]) extends Observable{
+
+  var gameStatus: GameStatus = INSERTPLAYER
+
   def createEmptyGrid(size:Int) : Unit = {
     grid = new Grid[Cell](size, Cell(Nil, Nil, Terrain(0), Fog(1)))
     notifyObservers()
   }
-
-  def addZombie(row: Int, col: Int): Unit = {
-    grid = grid.replaceCell(row, col, grid.cell(row, col).addZombie(Zombie(1,0)))
-    notifyObservers()
-  }
-
-//  def removeZombie(row: Int, col: Int): Unit = {
-//    grid = grid.replaceCell(row, col, grid.cell(row, col))
-//    notifyObservers()
-//  }
 
   def addPlayer(row: Int, col: Int, name: String): Unit = {
     grid = grid.replaceCell(row, col, grid.cell(row, col).addPlayer(Player(name)))
@@ -28,6 +22,17 @@ class Controller(var grid: Grid[Cell]) extends Observable{
     grid = grid.replaceCell(row, col, grid.cell(row, col).removePlayer(Player(name)))
     notifyObservers()
   }
+
+  def addZombie(row: Int, col: Int): Unit = {
+    grid = grid.replaceCell(row, col, grid.cell(row, col).addZombie(Zombie(1,0)))
+    notifyObservers()
+  }
+
+  //  def removeZombie(row: Int, col: Int): Unit = {
+  //    grid = grid.replaceCell(row, col, grid.cell(row, col))
+  //    notifyObservers()
+  //  }
+
 //
 //  def getPlayerPos(name: String): Unit = {
 //    grid.
---
+++ src/main/scala/de/htwg/se/seako/model/PlayerList.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.seako.model
+
+case class PlayerList(players: List[Player]) {
+
+  def addPlayer(player: Player): PlayerList = {
+    val newPlayers = players :+ player
+    this.copy(players = newPlayers)
+  }
+
+  def removePlayer(player: Player): PlayerList = {
+    val newPlayers = players.filterNot(players => players == player)
+    this.copy(players = newPlayers)
+  }
+
+  def getCurrentPlayer: Player = {
+    players.head
+  }
+
+}
